94b52907f10d6c662dfb39ed8f70332e7cbb270c
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-18 10:00:06 +0530

    Packege cut

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -13,12 +13,11 @@ render() {
   return (
     <div className='bg-orange-200 min-h-screen'>
       <h1 className='text-center text-red-600'>Fuck You..!</h1>
-      <h2 className='text-center text-cyan-50'>Fuck You..!</h2>
+      <h2 className='text-center text-cyan-500'>Fuck You..!</h2>
       <h3 className='text-center text-blue-500'>Fuck You..!</h3>
       <h4 className='text-center text-lime-500'>Fuck You..!</h4>
     </div>
     
   );
 }
-
 }
