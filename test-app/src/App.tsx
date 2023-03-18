3e23feda130c516f8d6002bff364c74b53d00d96
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-18 09:20:04 +0530

    Add h1 color

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -12,7 +12,10 @@ export default class App extends Component{
 render() {
   return (
     <div className='bg-orange-200 min-h-screen'>
-    
+      <h1 className='text-center text-red-600'>Fuck You..!</h1>
+      <h2 className='text-center text-cyan-50'>Fuck You..!</h2>
+      <h3 className='text-center text-blue-500'>Fuck You..!</h3>
+      <h4 className='text-center text-lime-500'>Fuck You..!</h4>
     </div>
     
   );
