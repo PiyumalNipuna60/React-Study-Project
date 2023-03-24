49abf5ab8ec048560c149b0b41cfbaa092005bb5
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-24 12:06:09 +0530

    add route and set path

--- test-app/src/components/Content/Content.tsx
+++ test-app/src/components/Content/Content.tsx
@@ -1,10 +1,20 @@
 import { Component } from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "../../pages/About";
+import Contact from "../../pages/Contact/Contact";
+import Home from "../../pages/Home";
+import Profile from "../../pages/profile/Profile";
 
 export default class Content extends Component {
   render() {
     return (
       <div className="mt-20">
-        
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+        </Routes>
       </div>
     );
   }
