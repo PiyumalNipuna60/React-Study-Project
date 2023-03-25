89b0de81736373b990b9aab72214e2155d056933
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-25 14:57:51 +0530

    Add icon

--- test-app/src/components/Card/Card.tsx
+++ test-app/src/components/Card/Card.tsx
@@ -13,8 +13,12 @@ export default class Card extends Component<CardProp,CardState> {
   render() {
     return (
       <div className="border border-slate-400 flex flex-col p-6 w-1/4 text-center rounded-2xl">
+        <span>
+        {this.props.icon}
       <h1 className="mb-2">{this.props.title}</h1>
+      </span>
       <p>{this.props.description}</p>
+        
       </div>
     )
   }
--- test-app/src/components/CardContainer/CardContainer.tsx
+++ test-app/src/components/CardContainer/CardContainer.tsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import Card from '../Card/Card'
 import AccessibilityIcon from '@mui/icons-material/Accessibility';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import AssistantIcon from '@mui/icons-material/Assistant';
 
 export default class CardContainer extends Component {
   render() {
     return (
         <div className="flex justify-center gap-6 p-4">
         <Card
-          icon=""
+          icon={<AccessibilityIcon/>}
           title="Card 1"
           description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
           dolores minus. Dolorem fugit pariatur dolorum. Maiores alias odit
@@ -15,6 +18,7 @@ export default class CardContainer extends Component {
           commodi animi."
         />
         <Card
+        icon={<AccountBalanceIcon />}
           title="Card 2"
           description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
           dolores minus. Dolorem fugit pariatur dolorum. Maiores alias odit
@@ -22,6 +26,7 @@ export default class CardContainer extends Component {
           commodi animi."
         />
         <Card
+        icon={<AnalyticsIcon />}
           title="Card 3"
           description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
           dolores minus. Dolorem fugit pariatur dolorum. Maiores alias odit
@@ -29,6 +34,7 @@ export default class CardContainer extends Component {
           commodi animi."
         />
         <Card
+        icon={<AssistantIcon />}
           title="Card 4"
           description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
           dolores minus. Dolorem fugit pariatur dolorum. Maiores alias odit
